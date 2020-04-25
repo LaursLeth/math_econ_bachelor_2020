cf597f2073ff2b48fe3fda80cbaaae103c0e0f5e
Laurs Randbøll Leth <llt@atp.dk>
2020-04-25 22:31:27 +0200

    Opdatering: BS-funktioner

--- code/Functions_BS.R
+++ code/Functions_BS.R
@@ -65,5 +65,25 @@ V.BS <- function(S,K,r,q,sigma,tau,option="Call"){
 }
 
 
-
+delta_hedge_error <- function(S,K,r,q,sigma, tau, call_price, strategy=-1,option="Call"){
+  
+  delta   <- BS.Delta(S=S,K=K,r=r,q=q, sigma=sigma,tau=tau, option=option)
+  
+  n            <- length(S[-length(S)])
+  
+  V            <- numeric(n+1); V[1] <- call_price[1] # Value process 
+  Pi           <- numeric(n+1);Pi[1]  <- 0  ### Path of PL/Hedging Error
+  dPi          <- numeric(n+1);dPi[1] <- 0  ### Hedging error over time interval
+  
+  if (length(r)==1){r<-rep(r,n)}
+  if (length(q)==1){q<-rep(q,n)}
+  
+  for (i in 1:n){
+    B       <- Pi[i] + strategy*delta[i]*S[i] - strategy*call_price[i] # Rebalance with money account
+    V[i+1] <- exp(r[i]*dt)*B - strategy*delta[i]*exp(q[i]*dt)*S[i+1] # Mark-to-market for portfolio
+    Pi[i+1] <- V[i+1] + strategy*call_price[i+1] # Mark-to-market for adjusted portfolio/cumulated hedging error
+    dPi[i+1] <- Pi[i+1] - Pi[i] # 
+  }
+  output=list(pf_value= V, error_cum = Pi, error_dt = dPi) # Save output
+}
 
